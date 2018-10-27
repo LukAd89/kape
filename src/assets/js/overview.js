86c1340c9ac2d61ce75e9c6cf605267fa4802835
unknown <mrskigh@gmail.com>
2018-10-27 15:58:26 +0200

    coderei

--- src/assets/js/overview.js
+++ src/assets/js/overview.js
@@ -2,32 +2,51 @@
 //var boxesData;
 var maxDates = 10;
 
+var reservationData = [{"ID": "1", "name": "laura", "boxID": "1"},{"ID": "2", "name": "finchen", "boxID": "2"}];
+
 $(function () {
     init();
 });
 
 const init = async () => {
-    fillCardsWithEmptySlots();
+    await fillCardsWithReservations();
+    await fillCardsWithEmptySlots();
     setDragzoneHandlers();
     setDropzoneHandlers();
 }
 
-const fillCardsWithEmptySlots = () => {
+const fillCardsWithReservations = () => {
     var dropzones = $('.dropzone');
 
-    console.log(dropzones);
+    dropzones.empty();
+
+    for(var i=0; i<reservationData.length; i++){
+        var reservation = reservationData[i];
+        var catSlot = '<li class="list-group-item">' + reservationSlot(reservation) + '</li>';
+        $('#boxlist' + reservation.boxID).append(catSlot);
+    }
+}
+
+const fillCardsWithEmptySlots = () => {
+    var emptyslot = '<li class="list-group-item"><div class="emptyslot">-</div></li>';
+    for(var i=1; i<=$('.dropzone').length; i++){
+        for(var j=0; j< $('#boxlist' + i).data('size'); j++){
+            $('#boxlist' + i).append(emptyslot)
+        }
+    }
 }
 
 const setDragzoneHandlers = () => {
     $(".dragzone").on("dragstart", function(ev) {
         ev.originalEvent.dataTransfer.setData("text", ev.originalEvent.target.id);
+        $(ev.originalEvent.target).closest('ul').find('.emptyslot').parent().addClass("border-success");
     });
 }
 
 const setDropzoneHandlers = () => {
     $(".dropzone")
         .on("dragenter", onDragEnter)
-        .on("dragover", onDragOver)
+        //.on("dragover", onDragOver)
         .on("dragleave", onDragLeave)
         .on("drop", onDrop);
 }
@@ -35,20 +54,31 @@ const setDropzoneHandlers = () => {
 const onDrop = function(ev) {
     ev.preventDefault();
     $(this).removeClass("border-success");
-    var data = ev.originalEvent.dataTransfer.getData("text");
+
     //ev.originalEvent.target.appendChild(document.getElementById(data));
 
     var dragID = ev.originalEvent.dataTransfer.getData("text");
-    var dropID =  ev.originalEvent.target.id;
 
-    console.log(dragID);
-    console.log(dropID);
+    if(!$('#' + dragID).hasClass('dragzone')){
+        console.log("no valid drag");
+        return;
+    }
+
+    var targetDropUL = $(ev.originalEvent.target).closest('ul');
+    $('#' + dragID).parent().append('<div class="emptyslot">-</div>');
+    $('#' + dragID).parent().appendTo($('#' + dragID).parent().parent());
+    $('#' + dragID).appendTo(targetDropUL.find('li').has('.emptyslot').first());
+    targetDropUL.find('div.emptyslot').first().remove();
+
+    return;
 
     if($(ev.originalEvent.target).hasClass('dropzone')){
         console.log("JOO");
         $('#' + dragID).appendTo($('#' + dropID));
     }else {
         //$('#' + dragID).appendTo($('.dropzone li').has('.emptyslot').first());
+        $('#' + dragID).parent().append('<div class="emptyslot">leer</div>');
+        $('#' + dragID).parent().appendTo($('#' + dragID).parent().parent());
         $('#' + dragID).appendTo($('#' + dropID).parent().parent().find('li').has('.emptyslot').first());
         $('#' + dropID).parent().parent().find('div.emptyslot').first().remove();
         //$('.dropzone li .e').has('.emptyslot').first().remove();
@@ -66,10 +96,13 @@ const onDragOver = function(ev) {
 
 const onDragEnter = function(ev) {
     ev.preventDefault();
-    $(this).addClass("border-success");
+    
 };
 
 const onDragLeave = function(ev) {
     ev.preventDefault();
-    $(this).removeClass("border-success");
+    $(ev.originalEvent.target).closest('ul').find('.emptyslot').parent().removeClass("border-success");
 };
+const reservationSlot = (reservation) => {
+    return '<div id="' + reservation.ID + '" class="dragzone" draggable="true">' + reservation.name + '</div>';
+}
